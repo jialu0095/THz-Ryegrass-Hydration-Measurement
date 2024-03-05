2f482121816e18b656a8649c78084ce4db831db0
Jialu Xing <jialu0095@gmail.com>
2024-03-05 16:34:59 +1300

    pixel value data issue

--- thz_combine_atnu_img.py
+++ thz_combine_atnu_img.py
@@ -9,26 +9,31 @@ serialPort = serial.Serial(port="COM4", baudrate=115200, bytesize=8, timeout=2, 
 print('COM4 is open:', serialPort.isOpen())
 
 # Query the current attenuation value
-serialPort.write(b':OUTP:ATT?\r')  # Assuming this is the command to query the attenuation value
+serialPort.write(b':OUTP:ATT?\r')  
 # Read the response
 
 # Initialize an instance of the processor class in single-threaded mode
 proc = processor.processor(threaded=False)
 proc.SetNorm(data=None)
+proc.SetExposure(3)
 
 res = serialPort.read(100)
 try:
     attenuation_value = float(res.decode().strip())  # response with attenuation value
     print("Current Attenuation:", attenuation_value, "dB")
 
-    data = proc.read()  # get img data
+    data = proc.read_raw()  # get img data
+    # data = proc.read()  # get img data
     print("Data shape:", data.shape)
     print("Max value:", data.max())
     print("Min value:", data.min())
-    # np.savetxt('thz_data_test.txt', data, fmt='%f')  
+    np.savetxt('thz_data_API.txt', data, fmt='%f')  
     
 except ValueError:
     print("Error reading attenuation value:", res.decode().strip())
 
 # Close the serial port
 serialPort.close()
+
+
+
